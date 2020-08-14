9c5a3572702adf5d8a9f97bde0d7c56a70e893c9
Diana <dianitavan@gmail.com>
2020-08-13 19:27:40 -0500

    activé el mongo

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 
-mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost:27017/visitantes', { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost:27017/mongo-1', { useNewUrlParser: true });
 
 const VisitorSchema = new mongoose.Schema({
     name: { type: String },
@@ -14,7 +14,7 @@ app.get("/", async(req, res) => {
     const visitor = new Visitor({ name: req.params.name || "Anónimo" });
     await visitor.save()
 
-    res.send("<h1>El visitante fue almacenado con éxito.</h1>")
+    res.send("<h1> El visitante fue almacenado con éxito </h1>")
 });
 
 app.listen(3000, () => console.log("Listening on port 3000 ..."));
